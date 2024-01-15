0564fe53b9feda4c1acdf82ee04fa444566a95de
Vaibhavsg03 <vaibhavproject4@gmail.com>
2024-01-16 01:41:37 +0530

    Subscribe section done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
-const Button = ({ label, iconURL, backgroundcolor, textcolor, bordercolor }) => {
+const Button = ({ label, iconURL, backgroundcolor, textcolor, bordercolor, fullWidth }) => {
     return (
         <button className={`flex justify-cetner items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
             ${backgroundcolor
                 ? `${backgroundcolor} ${textcolor} ${bordercolor}`
                 : "bg-coral-red text-white border-coral-red"
-            } rounded-full `}>
+            } rounded-full ${fullWidth && 'w-full'}'}`}>
             {label}
             {iconURL && < img src={iconURL} alt="ArrowRight icon" className="ml-2 rounded-full w-5 h-5" />}
         </button>
